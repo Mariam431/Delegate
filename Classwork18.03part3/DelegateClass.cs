f17a6ea76430413a4a23211b1b8a9de832dede23
mariam <mariam@MYPROBOOK>
2023-03-18 13:43:59 +0400

    Add project files.

---
+++ Classwork18.03part3/DelegateClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork18._03part3
+{
+    internal class DelegateClass
+    {
+        public delegate void DelegateName(int x); //Delegate
+        public void Meth1(int a) { Console.WriteLine("1"+a); } //Method
+        public void Meth2(int b) { Console.WriteLine("2"+b); } //Method
+        public void Meth3(int c) { Console.WriteLine("3"+c); } //Method
+
+
+        //ThreeMethodDelegate delegateObj = new ThreeMethodDelegate(Meth1);
+        //delegateObj += new ThreeMethodDelegate(Meth2);
+        //delegateObj += new ThreeMethodDelegate(Meth3);
+        //delegateObj();
+    }
+    //delegate int ThreeMethodDelegate(int t);
+}
---
+++ Classwork18.03part3/Program.cs
@@ -0,0 +1,30 @@
+﻿using static Classwork18._03part3.DelegateClass;
+namespace Classwork18._03part3
+
+{
+    internal class Program
+    {
+        public static void MethodG(int k) { Console.WriteLine(k); } //Method
+        static void Main(string[] args)
+        {
+            DelegateClass delegateClass = new DelegateClass(); //ClassInstance
+            DelegateName delegateName = new DelegateName(delegateClass.Meth1); //InstanceDelegate
+            delegateName += delegateClass.Meth2; //Add Methods
+            delegateName += delegateClass.Meth3;
+            delegateName += MethodG;
+            delegateName(5); //Parametrize Method
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
